04f5c2b1297109be1f60482d792755a6091509c9
Rupy <rupyapps@gmail.com>
2014-06-12 10:31:38 +0900

    利用サンプルを追加

--- main.rb
+++ main.rb
@@ -4,14 +4,25 @@ require "./scraping"
 
 ASSOCIATE_TAG = "********"
 ACCESS_KEY = "********"
-SECRET_KEY = "********" 
+SECRET_KEY = "********"
 scraping = AmazonBookScraping.new(ASSOCIATE_TAG, ACCESS_KEY, SECRET_KEY)
 
-# 全ページ数を取得
-puts scraping.total_pages("ruby")
-
-# ASINを取得
+# rubyというクエリに対するASINの配列を取得
 puts scraping.get_asin("ruby")
 
-# 和書に対して再帰的にカテゴリ名を取得する
-puts scraping.get_browsenode_info("465392")
+# 465392（和書）に対して書籍のasinを取得する
+puts scraping.get_asin_by_browsenode("465392")
+
+# 4774165166（パーフェクトRuby on Rails）に対して情報を取得する
+puts scraping.get_bookinfo_by_asin(4774165166)
+
+# 4774165166（パーフェクトRuby on Rails）に対して目次をスクレイピングで取得する
+puts scraping.get_contents(4774165166)
+
+# browsenode（本/ジャンル別/文学・評論/文芸作品/日本文学）の先祖の配列を取得する
+puts scraping.get_browsenode_ancestors(467250)
+
+# 465392（本）のカテゴリに対して再帰的にbrowsenodeをたどっていき，
+# 末尾のノードから得られるすべてのasinを用いて，目次を取得する
+scraping.create_table
+scraping.store_bookinfo_from_browsenode(465392, 507146)
